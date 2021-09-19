c0c8e9fb0db5a3b3d2bae13fd75ebc06c0e22a07
Donghyun <qhfhdls12@naver.com>
2021-09-19 21:20:06 +0900

    상체(pushup) 하체(squat)

--- exercise/print a number after enter.cpp
+++ exercise/print a number after enter.cpp
@@ -10,45 +10,101 @@
 
 using namespace std;
 
-void draw_Card()
-{
-
-}
-
-int main()
+void upper_body()
 {
 	cout << "세트수를 입력하시오 : ";
-	int doo = 0;
-	cin >> doo;
+	int set_num = 0;
+	cin >> set_num;
 
-
-	vector <int> card;
-
-	for (int i = 1; i < doo + 1; i++)
+	vector <int> set;
+	for (int i = 1; i < set_num + 1; i++)
 	{
-		card.push_back(i);
+		set.push_back(i);
 	}
 
-	vector<int>::iterator it;
-	srand(time(NULL));
+	vector<int>::iterator it; // it은 실행 할 횟수를 가르킴
 	char c;
 
-	for (int i = doo - 1; i >= 0; i--)
+	for (int i = 0; i < set_num; i++)
 	{
 		c = _getch();
 		if (c == 32 || c == 13)
 			// 13 == enter
 			// 32 == spacebar
 		{
-			it = card.begin();
+			if (i % 2 == 0)
+			{
+				it = set.end()-1; // end()는 마지막 원소의 다음을 가르킴
+				cout << *it << endl;
+				set.erase(it);
+			}
+			else
+			{
+				it = set.begin();
+				cout << *it << endl;
+				set.erase(it);
+			}
 
-			int num = rand() % (i + 1);
-			//printf("%d ", i);
-			//printf("%d ", num);
-			printf("%d\n", card[num]);
-			for (int i = 0; i < num; i++)
-				it++;
-			it = card.erase(it);
 		}
 	}
+
+
+}
+
+void squat()
+{
+	cout << "세트수를 입력하시오 : ";
+	int set_num = 0;
+	cin >> set_num;
+
+
+	vector <int> set;
+
+	for (int i = 1; i < set_num + 1; i++)
+	{
+		set.push_back(i);
+	}
+
+	vector<int>::iterator it; // it은 실행 할 횟수를 가르킴
+	srand(time(NULL));
+	char c;
+
+	for (int i = set_num - 1; i >= 0; i--)
+	{
+		c = _getch();
+		if (c == 32 || c == 13)
+			// 13 == enter
+			// 32 == spacebar
+		{
+			it = set.begin();
+
+			int num = rand() % (i + 1); // 랜덤 숫자 생성
+
+			printf("%d\n", set[num]);
+			for (int i = 0; i < num; i++)
+				it++;
+			set.erase(it);
+			// 랜덤 숫자 출력 및 벡테내의 해당 숫자 삭제
+		}
+	}
+}
+
+int main()
+{
+	cout << "---------------\n상체 : 1\n하체 : 2\n---------------\n";
+	cout << "운동할 부위를 선택하시오 : ";
+
+	int exercise;
+	cin >> exercise;
+
+	switch (exercise)
+	{
+	case 1:
+		upper_body();
+		break;
+	case 2:
+		squat();
+		break;
+	}
+
 }
